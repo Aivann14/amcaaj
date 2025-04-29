dc5088047f5cab776e0d792fb6da3e5d98247c4e
Aivan <aivaneborda12@gmail.com>
2025-04-29 11:18:40 +0800

    build: first commit to repo

---
+++ js/main.js
@@ -0,0 +1,143 @@
+/* 
+
+Vanilla Template
+
+https://templatemo.com/tm-526-vanilla
+
+*/
+
+jQuery(document).ready(function($) {
+
+	'use strict';
+
+    var top_header = $('.parallax-content');
+    top_header.css({'background-position':'center center'}); // better use CSS
+
+    $(window).scroll(function () {
+    var st = $(this).scrollTop();
+    top_header.css({'background-position':'center calc(50% + '+(st*.5)+'px)'});
+    });
+
+
+    $('body').scrollspy({ 
+        target: '.fixed-side-navbar',
+        offset: 200
+    });
+      
+      // smoothscroll on sidenav click
+
+    $('.tabgroup > div').hide();
+        $('.tabgroup > div:first-of-type').show();
+        $('.tabs a').click(function(e){
+          e.preventDefault();
+            var $this = $(this),
+            tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+            others = $this.closest('li').siblings().children('a'),
+            target = $this.attr('href');
+        others.removeClass('active');
+        $this.addClass('active');
+        $(tabgroup).children('div').hide();
+        $(target).show();
+      
+    })
+
+    var owl = $("#owl-testimonials");
+
+      owl.owlCarousel({
+        
+        pagination : true,
+        paginationNumbers: false,
+        autoPlay: 6000, //Set AutoPlay to 3 seconds
+        items : 3, //10 items above 1000px browser width
+        itemsDesktop : [1000,3], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,2], // betweem 900px and 601px
+        itemsTablet: [600,1], //2 items between 600 and 0
+        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+        
+    });
+
+
+});
+
+(function ($) {
+  "use strict";
+
+  // Spinner
+  var spinner = function () {
+      setTimeout(function () {
+          if ($('#spinner').length > 0) {
+              $('#spinner').removeClass('show');
+          }
+      }, 1);
+  };
+  spinner();
+  
+  
+  // Initiate the wowjs
+  new WOW().init();
+
+
+  // Sticky Navbar
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 45) {
+          $('.nav-bar').addClass('sticky-top');
+      } else {
+          $('.nav-bar').removeClass('sticky-top');
+      }
+  });
+  
+  
+  // Back to top button
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 300) {
+          $('.back-to-top').fadeIn('slow');
+      } else {
+          $('.back-to-top').fadeOut('slow');
+      }
+  });
+  $('.back-to-top').click(function () {
+      $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+      return false;
+  });
+
+
+  // Header carousel
+  $(".header-carousel").owlCarousel({
+      autoplay: true,
+      smartSpeed: 1500,
+      items: 1,
+      dots: true,
+      loop: true,
+      nav : true,
+      navText : [
+          '<i class="bi bi-chevron-left"></i>',
+          '<i class="bi bi-chevron-right"></i>'
+      ]
+  });
+
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+      autoplay: true,
+      smartSpeed: 1000,
+      margin: 24,
+      dots: false,
+      loop: true,
+      nav : true,
+      navText : [
+          '<i class="bi bi-arrow-left"></i>',
+          '<i class="bi bi-arrow-right"></i>'
+      ],
+      responsive: {
+          0:{
+              items:1
+          },
+          992:{
+              items:2
+          }
+      }
+  });
+  
+})(jQuery);
+
+
